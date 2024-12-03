ce2e247234499cf10633a307e776c600a33841e7
Rebmiami <rebmiami22@gmail.com>
2024-12-03 15:37:03 -0500

    Re-add RT60 energy graph, code cleanup

--- audiodata.py
+++ audiodata.py
@@ -9,5 +9,12 @@ class AudioData:
         # Perform analysis and store results
         self._audio_duration = analysis.get_duration(self)
         self._audio_resonance = analysis.get_resonance(self)
-        self._rt60 = analysis.compute_rt60(self)
+
+        low_filtered, mid_filtered, high_filtered = analysis.compute_bands(self)
+        energy_low, energy_mid, energy_high = analysis.compute_energy(low_filtered, mid_filtered, high_filtered, _sampling_rate)
+        rt60_low, rt60_mid, rt60_high = analysis.compute_rt60(energy_low, energy_mid, energy_high, _sampling_rate)
+
+        self._filtered = (low_filtered, mid_filtered, high_filtered)
+        self._energy = (energy_low, energy_mid, energy_high)
+        self._rt60 = (rt60_low, rt60_mid, rt60_high)
         pass
